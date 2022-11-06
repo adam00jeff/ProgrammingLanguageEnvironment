3c49ddf8a21c84b8b52290e17d2e27ac3c4d8377
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-11-06 12:51:04 +0000

    added multi-line functionality to methodised execute command, working

--- ProgrammingLanguageEnvironment/Execute.cs
+++ ProgrammingLanguageEnvironment/Execute.cs
@@ -13,28 +13,33 @@ namespace ProgrammingLanguageEnvironment
         public ArrayList shapes = new ArrayList();
         internal static object ExecuteParse(string inputtext, ArrayList shapes)
         {
-            var result = Parser.ParseInput(inputtext);
-            var input1 = result.Action;
-            var input2 = result.Paramaters.ToArray();
 
-            //shapes.Add(new Rectangle(Color.Black, 10, 100, 100, 150));
-
-
-
-            switch (input1)
+            string[] lines = inputtext.Split('\n');
+            foreach (string line in lines)
             {
-                case Action.Line:
-                    shapes.Add(new Line(Color.Black, 50, 50, input2[0], input2[1]));
-                    break;
-                case Action.Square:
-                    shapes.Add(new Rectangle(Color.Black, 50, 50, input2[0], input2[0]));
-                    break;
-                case Action.Circle:
-                    shapes.Add(new Circle(Color.Black, 70, 70, input2[0]));
-                    break;
-                case Action.Triangle:
-                    shapes.Add(new Triangle(Color.Black, 60, 60, input2[0], input2[1], input2[2]));
-                    break;
+                var result = Parser.ParseInput(line);
+                var input1 = result.Action;
+                var input2 = result.Paramaters.ToArray();
+
+                //shapes.Add(new Rectangle(Color.Black, 10, 100, 100, 150));
+
+
+
+                switch (input1)
+                {
+                    case Action.Line:
+                        shapes.Add(new Line(Color.Black, 50, 50, input2[0], input2[1]));
+                        break;
+                    case Action.Square:
+                        shapes.Add(new Rectangle(Color.Black, 50, 50, input2[0], input2[0]));
+                        break;
+                    case Action.Circle:
+                        shapes.Add(new Circle(Color.Black, 70, 70, input2[0]));
+                        break;
+                    case Action.Triangle:
+                        shapes.Add(new Triangle(Color.Black, 60, 60, input2[0], input2[1], input2[2]));
+                        break;
+                }
             }
 
             return shapes;
