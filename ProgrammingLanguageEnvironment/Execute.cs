c8586e360b5869acbde6b6dec8884e11b1f0a224
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-11-06 12:58:04 +0000

    changed default location from a static value to a variable to allow additional functionality such as MoveTo

--- ProgrammingLanguageEnvironment/Execute.cs
+++ ProgrammingLanguageEnvironment/Execute.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +12,13 @@ namespace ProgrammingLanguageEnvironment
     public class Execute
     {
         public ArrayList shapes = new ArrayList();
+        
+
+
         internal static object ExecuteParse(string inputtext, ArrayList shapes)
         {
-
+            int xDef = 0;
+            int yDef = 0;
             string[] lines = inputtext.Split('\n');
             foreach (string line in lines)
             {
@@ -21,23 +26,19 @@ namespace ProgrammingLanguageEnvironment
                 var input1 = result.Action;
                 var input2 = result.Paramaters.ToArray();
 
-                //shapes.Add(new Rectangle(Color.Black, 10, 100, 100, 150));
-
-
-
                 switch (input1)
                 {
                     case Action.Line:
-                        shapes.Add(new Line(Color.Black, 50, 50, input2[0], input2[1]));
+                        shapes.Add(new Line(Color.Black, xDef, yDef, input2[0], input2[1]));
                         break;
                     case Action.Square:
-                        shapes.Add(new Rectangle(Color.Black, 50, 50, input2[0], input2[0]));
+                        shapes.Add(new Rectangle(Color.Black, xDef, yDef, input2[0], input2[0]));
                         break;
                     case Action.Circle:
-                        shapes.Add(new Circle(Color.Black, 70, 70, input2[0]));
+                        shapes.Add(new Circle(Color.Black, xDef, yDef, input2[0]));
                         break;
                     case Action.Triangle:
-                        shapes.Add(new Triangle(Color.Black, 60, 60, input2[0], input2[1], input2[2]));
+                        shapes.Add(new Triangle(Color.Black, xDef, yDef, input2[0], input2[1], input2[2]));
                         break;
                 }
             }
