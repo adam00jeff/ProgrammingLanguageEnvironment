db728835d62aa658db797d3e80f9647b3ec8b5ea
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-11-07 18:07:22 +0000

    added .xml comments to the line class

--- ProgrammingLanguageEnvironment/Line.cs
+++ ProgrammingLanguageEnvironment/Line.cs
@@ -7,25 +7,44 @@ using System.Threading.Tasks;
 
 namespace ProgrammingLanguageEnvironment
 {
+    /// <summary>
+    /// class to draw lines 
+    /// line method called from DrawTo
+    /// </summary>
     class Line : Shape
     {
-        int tox, toy;
-
+        int tox, toy; // ending position of the line
+        /// <summary>
+        /// constructor for lines
+        /// </summary>
+        /// <param name="colour">the colour of the line</param>
+        /// <param name="x">starting x axis position</param>
+        /// <param name="y">starting y axis position</param>
+        /// <param name="tox">ending x axis position</param>
+        /// <param name="toy">ending y axis position</param>
         public Line(Color colour, int x, int y, int tox,int toy) : base(colour, x, y)
         {
             this.tox = tox;
             this.toy = toy;
         }
-
+        /// <summary>
+        /// draws a line on the graphics object
+        /// </summary>
+        /// <param name="g">graphics object to be output</param>
         public override void draw(Graphics g)
         {
-            Pen p = new Pen(colour, 2);
-            g.DrawLine(p, x, y, tox, toy);
+            Pen p = new Pen(colour, 2);//creates a new pen of chosen colour
+            g.DrawLine(p, x, y, tox, toy);//draws a line from starting position to new position
         }
+        /// <summary>
+        /// method to call when fill is selected
+        /// does not change the lines
+        /// </summary>
+        /// <param name="g">the graphics object to be output</param>
         public override void drawfilled(Graphics g)
         {
-            Pen p = new Pen(colour, 2);
-            g.DrawLine(p, x, y, tox, toy);
+            Pen p = new Pen(colour, 2);//creates a new pen of chosen colour
+            g.DrawLine(p, x, y, tox, toy);//draws a line from starting position to new position
         }
 
     }
