3b7901cb086126ca9fe86c28c10c99ba5f47b5ee
Adam Jeffreson <adam.jeffreson@gmail.com>
2023-01-05 12:00:41 +0000

    layout changes for feedback box on form

--- ProgrammingLanguageEnvironment/Form1.Designer.cs
+++ ProgrammingLanguageEnvironment/Form1.Designer.cs
@@ -48,6 +48,9 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.OutputWindow)).BeginInit();
             this.SuspendLayout();
             // 
@@ -77,7 +80,7 @@
             this.OutputWindow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.OutputWindow.Location = new System.Drawing.Point(505, 46);
             this.OutputWindow.Name = "OutputWindow";
-            this.OutputWindow.Size = new System.Drawing.Size(394, 564);
+            this.OutputWindow.Size = new System.Drawing.Size(570, 564);
             this.OutputWindow.TabIndex = 2;
             this.OutputWindow.TabStop = false;
             this.OutputWindow.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -125,7 +128,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(477, 49);
+            this.label1.Location = new System.Drawing.Point(477, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(22, 13);
             this.label1.TabIndex = 7;
@@ -233,17 +236,47 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(505, 27);
+            this.label12.Location = new System.Drawing.Point(505, 30);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(42, 13);
             this.label12.TabIndex = 20;
             this.label12.Text = "Output:";
             // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(471, 530);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "0,500";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(886, 30);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(34, 13);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "400,0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(988, 30);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 13);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "500,0";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(915, 666);
+            this.ClientSize = new System.Drawing.Size(1087, 666);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -296,6 +329,9 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
     }
 }
 
